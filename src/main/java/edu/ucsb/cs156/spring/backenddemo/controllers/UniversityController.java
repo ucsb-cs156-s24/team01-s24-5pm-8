ee8605bd6f806303dbadd8c047af8c8f909e939e
Brady Gin <bradygin3@gmail.com>
2024-04-23 01:12:41 -0700

    bg - Added test for UniversityController and modified documentation description

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
-@Tag(name="University info from USGS")
+@Tag(name="University info from http://universities.hipolabs.com/search")
 @Slf4j
 @RestController
 @RequestMapping("/api/university")
@@ -27,10 +27,10 @@ public class UniversityController {
     @Autowired
     UniversityQueryService universityQueryService;
 
-    @Operation(summary = "Enter part of a university name, get back all matching universities", description = "Uses API documented here: http://universities.hipolabs.com/search")
+    @Operation(summary = "Enter part of a university name, get back all matching universities", description = "Uses list from here: http://universities.hipolabs.com/search")
     @GetMapping("/get")
     public ResponseEntity<String> getUniversity(
-        @Parameter(name="name", description="name of University", example="Harvard") @RequestParam String name
+        @Parameter(name="name", description="name or part of name of university", example="Harvard") @RequestParam String name
     ) throws JsonProcessingException {
         log.info("getUniversity: name{}", name);
         String result = universityQueryService.getJSON(name);
